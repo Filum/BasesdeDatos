28490ca8f1ac9d1eb877b6ce07cddba1d4a4c3d5
Charlin Arroyo Núñez <charlindayana@gmail.com>
2018-10-22 15:41:30 -0600

    Ventanas

---
+++ Proyecto Bases de Datos/Seguridad.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_Bases_de_Datos
+{
+    /// <summary>
+    /// Lógica de interacción para Seguridad.xaml
+    /// </summary>
+    public partial class Seguridad : Window
+    {
+        public Seguridad()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_menuS_Click(object sender, RoutedEventArgs e)
+        {
+            Menu menu = new Menu();
+            menu.Show();
+            this.Close();
+        }
+    }
+}
