bbd62fb032a8322b6357fdbbd4a2c23e205c669d
Edgardo <edgardoacu@hotmail.com>
2018-10-22 14:13:22 -0600

    Conexion a base de datos

---
+++ Proyecto Bases de Datos/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Bases_de_Datos
+{
+    class Conexion
+    {
+        public string contraseña { set; get; }
+    }
+}
--- Proyecto Bases de Datos/MainWindow.xaml.cs
+++ Proyecto Bases de Datos/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Oracle.DataAccess.Client;
+using System.Data;
+
 
 namespace Proyecto_Bases_de_Datos
 {
@@ -23,6 +26,33 @@ namespace Proyecto_Bases_de_Datos
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void conectar_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            try
+            {
+
+                OracleConnection conn = new OracleConnection("DATA SOURCE=localhost:1521/XE;PERSIST SECURITY INFO=True;USER ID=DELRAM;PASSWORD ="+ tbx_contraseña.Text);
+                conn.Open();
+                MessageBox.Show("conectado correctamente a la base de datos");
+                Conexion contraseña = new Conexion();
+                contraseña.contraseña = tbx_contraseña.Text;
+                tablespaces tab = new tablespaces();
+                tab.Show();
+                this.Close();
+                conn.Close();
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No se pudo establecer una conexión con la base de datos","Error");
+            }
+
+
+            
         }
     }
 }
