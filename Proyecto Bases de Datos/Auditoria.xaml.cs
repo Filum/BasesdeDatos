327b87ee6cc82695eafe9ea496e02117fc04c71f
Edgardo <edgardoacu@hotmail.com>
2018-11-06 01:06:18 -0600

    Creacion de Respaldos-Edgardo

--- Proyecto Bases de Datos/Auditoria.xaml.cs
+++ Proyecto Bases de Datos/Auditoria.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Oracle.DataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -33,7 +35,43 @@ namespace Proyecto_Bases_de_Datos
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                OracleConnection conn = DataBase.Conexion();
+                conn.Open();
+                OracleCommand comando = new OracleCommand();
+                comando.Connection = conn;
+                comando.CommandText = "Select name, value from v$parameter where name like 'audit_trail' ";
+                OracleDataAdapter adaptador = new OracleDataAdapter();
+                adaptador.SelectCommand = comando;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                tabla_datos.ItemsSource = tabla.DefaultView;
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error al mostrar datos de la tabla");
+            }
+        }
 
+        private void button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OracleConnection conn = DataBase.Conexion();
+                conn.Open();
+                OracleCommand comando = new OracleCommand();
+                comando.Connection = conn;
+                comando.CommandText = " Alter system set audit_trail =DB scope = spfile";
+                OracleDataReader dr = comando.ExecuteReader();
+                conn.Close();
+                MessageBox.Show("Comando enviado");
+            }
+            catch
+            {
+                MessageBox.Show("Error enviar el comando");
+            }
         }
     }
 }
