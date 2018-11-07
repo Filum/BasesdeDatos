47283d596bdb34a2565a98f14e760068c13381c9
Edgardo <edgardoacu@hotmail.com>
2018-11-07 13:59:06 -0600

    Funcionalidad en recuperar Schemas y tablas-Edgardo

--- Proyecto Bases de Datos/Respaldos.xaml.cs
+++ Proyecto Bases de Datos/Respaldos.xaml.cs
@@ -24,7 +24,7 @@ namespace Proyecto_Bases_de_Datos
         public Respaldos()
         {
             InitializeComponent();
-            
+
         }
 
         private void btn_menuS_Click(object sender, RoutedEventArgs e)
@@ -43,8 +43,8 @@ namespace Proyecto_Bases_de_Datos
                 conn.Open();
                 OracleCommand comando = new OracleCommand();
                 comando.Connection = conn;
-                comando.CommandText = " CREATE OR REPLACE DIRECTORY RESPALDO AS '"+ txt_directorio.Text+ "'";
-                OracleDataReader dr = comando.ExecuteReader();MessageBox.Show(comando.CommandText);
+                comando.CommandText = " CREATE OR REPLACE DIRECTORY RESPALDO AS '" + txt_directorio.Text + "'";
+                OracleDataReader dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
                 comando.CommandText = "GRANT READ, WRITE ON DIRECTORY RESPALDO TO SYSTEM";
                 dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
                 conn.Close();
@@ -55,7 +55,7 @@ namespace Proyecto_Bases_de_Datos
             }
 
             System.Diagnostics.Process p = new System.Diagnostics.Process();
-            string strexp = "EXPDP SYSTEM/"+txt_contraseña.Text+"@XE FULL=Y DIRECTORY=RESPALDO DUMPFILE="+txt_nombreArchivo.Text+".DMP LOGFILE=" + txt_nombreArchivo.Text + ".LOG;";
+            string strexp = "EXPDP SYSTEM/" + txt_contraseña.Text + "@XE FULL=Y DIRECTORY=RESPALDO DUMPFILE=" + txt_nombreArchivo.Text + ".DMP LOGFILE=EXPDP" + txt_nombreArchivo.Text + ".LOG;";
             p.StartInfo.FileName = "cmd.exe";
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardInput = true;
@@ -67,12 +67,12 @@ namespace Proyecto_Bases_de_Datos
             try
             {
                 p.Start();
-                p.StandardInput.WriteLine(strexp); 
+                p.StandardInput.WriteLine(strexp);
                 p.StandardInput.WriteLine("Exit");
                 p.WaitForExit(1000);
                 MessageBox.Show("cerre el processo");
             }
-            catch 
+            catch
             {
                 MessageBox.Show("ERROR AL CREAR EL RESPALDO");
             }
@@ -92,11 +92,12 @@ namespace Proyecto_Bases_de_Datos
                 adaptador.SelectCommand = comando;
                 DataTable tabla = new DataTable();
                 adaptador.Fill(tabla);
-                
-                foreach(DataRow fila in tabla.Rows)
+
+                foreach (DataRow fila in tabla.Rows)
                 {
                     cbox_tabla.Items.Add(Convert.ToString(fila["TABLE_NAME"]));
-                }conn.Close();
+                }
+                conn.Close();
             }
             catch
             {
@@ -109,6 +110,7 @@ namespace Proyecto_Bases_de_Datos
             try
             {
                 cbox_schema.Items.Clear();
+
                 OracleConnection conn = DataBase.Conexion();
                 conn.Open();
                 OracleCommand comando = new OracleCommand();
@@ -121,6 +123,7 @@ namespace Proyecto_Bases_de_Datos
                 foreach (DataRow fila in tabla.Rows)
                 {
                     cbox_schema.Items.Add(Convert.ToString(fila["OWNER"]));
+
                 }
 
                 conn.Close();
@@ -131,7 +134,57 @@ namespace Proyecto_Bases_de_Datos
             }
         }
 
-        
+        private void cargarDatos1(object sender, EventArgs e)
+        {
+            try
+            {
+                cbox_schemaRecuperacion.Items.Clear();
+                OracleConnection conn = DataBase.Conexion();
+                conn.Open();
+                OracleCommand comando = new OracleCommand();
+                comando.Connection = conn;
+                comando.CommandText = "select distinct owner from dba_objects";
+                OracleDataAdapter adaptador = new OracleDataAdapter();
+                adaptador.SelectCommand = comando;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                foreach (DataRow fila in tabla.Rows)
+                {
+                    cbox_schemaRecuperacion.Items.Add(Convert.ToString(fila["OWNER"]));
+                }
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error al actualizar datos de la tabla");
+            }
+        }
+
+        private void cargarDatos2(object sender, EventArgs e)
+        {
+            try
+            {
+                cbox_UsuarioRespaldo.Items.Clear();
+                OracleConnection conn = DataBase.Conexion();
+                conn.Open();
+                OracleCommand comando = new OracleCommand();
+                comando.Connection = conn;
+                comando.CommandText = "select username from all_users";
+                OracleDataAdapter adaptador = new OracleDataAdapter();
+                adaptador.SelectCommand = comando;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+                foreach (DataRow fila in tabla.Rows)
+                {
+                    cbox_UsuarioRespaldo.Items.Add(Convert.ToString(fila["USERNAME"]));
+                }
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error al actualizar datos de la tabla");
+            }
+        }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
@@ -144,7 +197,7 @@ namespace Proyecto_Bases_de_Datos
                 comando.Connection = conn;
                 comando.CommandText = " CREATE OR REPLACE DIRECTORY RESPALDO AS '" + txt_directorio.Text + "'";
                 OracleDataReader dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
-                comando.CommandText = "GRANT READ, WRITE ON DIRECTORY RESPALDO TO "+cbox_schema.SelectedItem.ToString();
+                comando.CommandText = "GRANT READ, WRITE ON DIRECTORY RESPALDO TO " + cbox_schema.SelectedItem.ToString();
                 dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
                 conn.Close();
             }
@@ -154,7 +207,7 @@ namespace Proyecto_Bases_de_Datos
             }
 
             System.Diagnostics.Process p = new System.Diagnostics.Process();
-            string strexp = "EXPDP "+cbox_schema.SelectedItem.ToString()+"/" + txt_contraseña.Text + "@XE SCHEMAS=" + cbox_schema.SelectedItem.ToString() + " DIRECTORY=RESPALDO DUMPFILE=" + txt_nombreArchivo.Text + ".DMP LOGFILE=" + txt_nombreArchivo.Text + ".LOG;";
+            string strexp = "EXPDP " + cbox_schema.SelectedItem.ToString() + "/" + txt_contraseña.Text + "@XE SCHEMAS=" + cbox_schema.SelectedItem.ToString() + " DIRECTORY=RESPALDO DUMPFILE=" + txt_nombreArchivo.Text + ".DMP LOGFILE=EXPDP" + txt_nombreArchivo.Text + ".LOG;";
             p.StartInfo.FileName = "cmd.exe";
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardInput = true;
@@ -197,7 +250,7 @@ namespace Proyecto_Bases_de_Datos
             }
 
             System.Diagnostics.Process p = new System.Diagnostics.Process();
-            string strexp = "EXPDP " + cbox_schema.SelectedItem.ToString() + "/" + txt_contraseña.Text + "@XE TABLES=" + cbox_schema.SelectedItem.ToString() + "."+cbox_tabla.SelectedItem.ToString()+" DIRECTORY=RESPALDO DUMPFILE=" + txt_nombreArchivo.Text + ".DMP LOGFILE=" + txt_nombreArchivo.Text + ".LOG;";
+            string strexp = "EXPDP " + cbox_schema.SelectedItem.ToString() + "/" + txt_contraseña.Text + "@XE TABLES=" + cbox_schema.SelectedItem.ToString() + "." + cbox_tabla.SelectedItem.ToString() + " DIRECTORY=RESPALDO DUMPFILE=" + txt_nombreArchivo.Text + ".DMP LOGFILE=EXPDP" + txt_nombreArchivo.Text + ".LOG;";
             p.StartInfo.FileName = "cmd.exe";
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardInput = true;
@@ -218,6 +271,146 @@ namespace Proyecto_Bases_de_Datos
             {
                 MessageBox.Show("ERROR AL CREAR EL RESPALDO");
             }
+        }
+
+        private void cbox_schemaRecuperacion_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                cbox_tablaRecuperacion.Items.Clear();
+                OracleConnection conn = DataBase.Conexion();
+                conn.Open();
+                OracleCommand comando = new OracleCommand();
+                comando.Connection = conn;
+                comando.CommandText = "SELECT TABLE_NAME FROM DBA_TABLES WHERE OWNER = '" + cbox_schemaRecuperacion.SelectedItem.ToString() + "'";
+                OracleDataAdapter adaptador = new OracleDataAdapter();
+                adaptador.SelectCommand = comando;
+                DataTable tabla = new DataTable();
+                adaptador.Fill(tabla);
+
+                foreach (DataRow fila in tabla.Rows)
+                {
+                    cbox_tablaRecuperacion.Items.Add(Convert.ToString(fila["TABLE_NAME"]));
+                }
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Error al actualizar datos de la tabla");
+            }
+        }
+
+        
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                OracleConnection conn = DataBase.Conexion();
+                conn.Open();
+                OracleCommand comando = new OracleCommand();
+                comando.Connection = conn;
+                comando.CommandText = " CREATE OR REPLACE DIRECTORY RESPALDO AS '" + txt_directorioRecuperacion.Text + "'";
+                OracleDataReader dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
+                comando.CommandText = "GRANT READ, WRITE ON DIRECTORY RESPALDO TO " + cbox_UsuarioRespaldo.SelectedItem.ToString();
+                dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
+                comando.CommandText = "DROP TABLE " + cbox_schemaRecuperacion.SelectedItem.ToString() + "." + cbox_tablaRecuperacion.SelectedItem.ToString(); MessageBox.Show("TABLA BORRADA");
+                dr = comando.ExecuteReader();
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("ERROR AL ASIGNAR PERMISOS DE LECTURA Y ESCRITURA");
+            }
+
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            string strexp = "IMPDP " + cbox_UsuarioRespaldo.SelectedItem.ToString() + "/" + txt_contraseñaRecuperacion.Text + "@XE TABLES=" + cbox_schemaRecuperacion.SelectedItem.ToString() + "." + cbox_tablaRecuperacion.SelectedItem.ToString() + " DIRECTORY=RESPALDO DUMPFILE=" + txt_nombreArchivoRecuperacion.Text + ".DMP LOGFILE=IMPDP" + txt_nombreArchivoRecuperacion.Text + ".LOG"; MessageBox.Show(strexp);
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.EnableRaisingEvents = true;
+            p.StartInfo.CreateNoWindow = true;
+
+            try
+            {
+                p.Start();
+                p.StandardInput.WriteLine(strexp);
+                p.StandardInput.WriteLine("Exit");
+                p.WaitForExit(1000);
+                MessageBox.Show("TABLA RECUPERADA");
+            }
+            catch
+            {
+                MessageBox.Show("ERROR AL CREAR EL RESPALDO");
+
+            }
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OracleConnection conn = DataBase.Conexion();
+                conn.Open();
+                OracleCommand comando = new OracleCommand();
+                comando.Connection = conn;
+
+                comando.CommandText = " CREATE OR REPLACE DIRECTORY RESPALDO AS '" + txt_directorioRecuperacion.Text + "'";
+                OracleDataReader dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
+
+                comando.CommandText = "GRANT READ, WRITE ON DIRECTORY RESPALDO TO " + cbox_schemaRecuperacion.SelectedItem.ToString();MessageBox.Show(comando.CommandText);
+                dr = comando.ExecuteReader(); 
+
+                comando.CommandText = "DROP USER HR CASCADE"; MessageBox.Show(comando.CommandText);
+                dr = comando.ExecuteReader(); 
+
+                comando.CommandText = "CREATE USER "+ cbox_UsuarioRespaldo.SelectedItem.ToString() + " IDENTIFIED BY "+txt_contraseñaRecuperacion.Text+" DEFAULT TABLESPACE USERS TEMPORARY TABLESPACE TEMP";//falta agregar para diferentes tablespace o diferentes temporarys
+                dr = comando.ExecuteReader(); MessageBox.Show(comando.CommandText);
+
+                comando.CommandText = "GRANT CONNECT , RESOURCE, DBA TO "+ cbox_UsuarioRespaldo.SelectedItem.ToString(); MessageBox.Show(comando.CommandText);
+                dr = comando.ExecuteReader();
+
+                comando.CommandText = "GRANT ALL PRIVILEGES TO " + cbox_UsuarioRespaldo.SelectedItem.ToString(); MessageBox.Show(comando.CommandText);
+                dr = comando.ExecuteReader();
+                
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("ERROR AL ASIGNAR PERMISOS DE LECTURA Y ESCRITURA");
+            }
+
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            string strexp = "IMPDP "+ cbox_schemaRecuperacion.SelectedItem.ToString() + "/"+txt_contraseñaRecuperacion.Text+"@XE SCHEMAS="+ cbox_schemaRecuperacion.SelectedItem.ToString() + " DIRECTORY=RESPALDO DUMPFILE="+txt_nombreArchivoRecuperacion.Text+".DMP LOGFILE=IMPDP"+ txt_nombreArchivoRecuperacion.Text + ".LOG"; MessageBox.Show(strexp);
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.EnableRaisingEvents = true;
+            p.StartInfo.CreateNoWindow = true;
+
+            try
+            {
+                p.Start();
+                p.StandardInput.WriteLine(strexp);
+                p.StandardInput.WriteLine("Exit");
+                p.WaitForExit(1000);
+                MessageBox.Show("TABLA RECUPERADA");
+            }
+            catch
+            {
+                MessageBox.Show("ERROR AL CREAR EL RESPALDO");
+
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
     
